124d795ee65f7e3afab52c916f5ede47852bfabe
Kuda <kuda2012@gmail.com>
2020-09-02 01:35:19 -0500

    second commit

--- insertion.js
+++ insertion.js
@@ -1,3 +1,16 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      let smallerArr = arr.slice(0, i + 1);
+      for (let j = 0; j < smallerArr.length; j++) {
+        if (arr[i] < smallerArr[j]) {
+          arr[j] = arr[i];
+          arr[i] = smallerArr[j];
+        }
+      }
+    }
+  }
+  return arr;
+}
 
 module.exports = insertionSort;
--- selection.js
+++ selection.js
@@ -1,3 +1,16 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowestSoFar;
+    for (let j = i; j < arr.length; j++) {
+      lowestSoFar = arr[i];
+      if (arr[j] < lowestSoFar) {
+        let temp = lowestSoFar;
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
 
 module.exports = selectionSort;

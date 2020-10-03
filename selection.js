5669ad9d87a81a1e36f60369e135e347dfac61c1
kuda2012 <kuda2012@gmail.com>
2020-10-03 18:37:23 -0500

    second commit

--- selection.js
+++ selection.js
@@ -2,7 +2,6 @@ function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let lowestSoFar;
     for (let j = i; j < arr.length; j++) {
-      lowestSoFar = arr[i];
       if (arr[j] < lowestSoFar) {
         let temp = lowestSoFar;
         arr[i] = arr[j];
